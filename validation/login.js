a6455a10bd91770ab765cba6b4b6251f898fe2df
Egor <egorzvuzdetskyi@gmail.com>
2018-10-28 15:36:16 +0200

    fixed is-empty method fixed login validator

--- validation/login.js
+++ validation/login.js
@@ -4,15 +4,6 @@ const Validator = require('validator'),
 module.exports = validateLoginInput = data => {
     let errors = {};
 
-    if (
-        !Validator.isLength(data.name, {
-                min: 2,
-                max: 30
-            }
-        )
-    ) {
-        errors.name = 'Name must be between 2 and 30 characters'
-    }
     data.password = !isEmpty(data.password) ? data.password : '';
     data.email = !isEmpty(data.email) ? data.email : '';
 

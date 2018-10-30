0d8312fe9ee9e33d6a66411e3bd055238b492e71
Egor <egorzvuzdetskyi@gmail.com>
2018-10-30 07:48:38 +0200

    fixed register validation fixed login validation

--- validation/register.js
+++ validation/register.js
@@ -35,7 +35,7 @@ module.exports = validateRegisterInput = data => {
         errors.password = 'Password field is required';
     }
 
-    if (Validator.isLength(data.email, { min: 6, max: 30 })) {
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
         errors.password = 'Password must be between 6 and 30 characters'
     }
 

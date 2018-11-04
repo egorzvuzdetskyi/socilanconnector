0c099f795a9c34078f49aa2640a847246a295016
Egor <egorzvuzdetskyi@gmail.com>
2018-11-04 15:43:08 +0200

    updated links for react-router-dom

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from "react-router-dom"
 
 class Landing extends Component {
     render() {
@@ -14,8 +15,8 @@ class Landing extends Component {
                                     Create profile for communicate with another people. Life. Speak. Enjoy.
                                 </p>
                                 <hr/>
-                                <a href="register.html" className="btn btn-lg btn-info mr-2">Sign Up</a>
-                                <a href="login.html" className="btn btn-lg btn-light">Login</a>
+                                <Link to="register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
+                                <Link to="login" className="btn btn-lg btn-light">Login</Link>
                             </div>
                         </div>
                     </div>

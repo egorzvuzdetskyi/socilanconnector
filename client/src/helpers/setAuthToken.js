9dd715841a487eb3f7cce0d6b1c4b8808510e640
Egor <egorzvuzdetskyi@gmail.com>
2018-11-06 07:36:17 +0200

    added setAuthToken function

---
+++ client/src/helpers/setAuthToken.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const setAuthToken = token => {
+    if(!token) {
+        delete axios.defaults.headers.common['Authorization']
+    };
+
+    axios.defaults.headers.common['Authorization'] = token;
+}

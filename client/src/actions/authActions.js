581e6f2f0ab92b54aa8e4126e99c3f583e851e95
Egor <egorzvuzdetskyi@gmail.com>
2018-11-06 07:37:48 +0200

    added redux logic of login

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
-import {GET_ERRORS} from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import {
+    setAuthToken,
+    getLocalStorageName
+} from "../helpers";
+import jwt_decode from 'jwt-decode'
 
 export const registerUser = (userData, history) => dispatch => {
 
     axios
         .post('/api/users/register', userData)
-        .then(user => {;
+        .then(user => {
             history.push('/login');
         })
         .catch(
@@ -17,3 +22,32 @@ export const registerUser = (userData, history) => dispatch => {
             }
         );
 };
+
+export const loginUser = (userData) => dispatch => {
+    axios.post('/api/users/login', userData)
+        .then(res => {
+            console.log(res);
+            const {
+                token
+            } = res.data;
+            localStorage.setItem(getLocalStorageName('token'), token);
+            setAuthToken(token);
+            const decoded = jwt_decode(token);
+            dispatch(setCurrentUser(decoded))
+        })
+        .catch(
+            error => {
+                return dispatch({
+                    type: GET_ERRORS,
+                    payload: error.response.data
+                })
+            }
+        );
+};
+
+export const setCurrentUser = decoded => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}

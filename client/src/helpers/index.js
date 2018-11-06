ac30bc6a89a7f1b7d7430a80f10e2c7ebcb0226c
Egor <egorzvuzdetskyi@gmail.com>
2018-11-06 07:37:21 +0200

    added helpers facade.

---
+++ client/src/helpers/index.js
@@ -0,0 +1,9 @@
+import { setAuthToken } from "./setAuthToken";
+import { getLocalStorageName } from "./localStorageName.map";
+import { isEmpty } from "./is-empty";
+
+export {
+    setAuthToken,
+    getLocalStorageName,
+    isEmpty
+}

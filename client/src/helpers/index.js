637bda640980d1126588f172e1251a0f97c1006f
Egor <egorzvuzdetskyi@gmail.com>
2018-11-06 09:19:21 +0200

    added isTokenExpired method into facade.

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,9 +1,11 @@
 import { setAuthToken } from "./setAuthToken";
 import { getLocalStorageName } from "./localStorageName.map";
 import { isEmpty } from "./is-empty";
+import { isTokenExpired } from "./isTokenExpired";
 
 export {
     setAuthToken,
     getLocalStorageName,
-    isEmpty
+    isEmpty,
+    isTokenExpired
 }

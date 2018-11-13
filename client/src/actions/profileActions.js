8ed466811f354960c1b07b0fa1ecc708e972be25
Egor <egorzvuzdetskyi@gmail.com>
2018-11-13 15:33:06 +0200

    added deleteExperience method to actions

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -90,3 +90,20 @@ export const addEducation = (eduData, history) => dispatch => {
             })
         })
 };
+
+export const deleteExperience = (id) => dispatch => {
+    axios
+        .delete(`/api/profile/experience/${id}`)
+        .then(res => {
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            });
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
+};

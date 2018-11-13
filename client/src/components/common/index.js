c1d54e1a6e4484dbbab6ca8af04ea5b04c595dd6
Egor <egorzvuzdetskyi@gmail.com>
2018-11-13 08:12:42 +0200

    added fasade for common components

---
+++ client/src/components/common/index.js
@@ -0,0 +1,15 @@
+import InputGroup from "./InputGroup";
+import PrivateRoute from "./PrivateRoute";
+import SelectListGroup from "./SelectListGroup";
+import Spinner from "./Spinner";
+import TextFieldGroup from "./TextFieldGroup";
+import TextAreaGroup from "./TextAreaField";
+
+export {
+    InputGroup,
+    PrivateRoute,
+    SelectListGroup,
+    Spinner,
+    TextAreaGroup,
+    TextFieldGroup
+}

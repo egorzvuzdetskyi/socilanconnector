79c03f2f7f84db29557d18f638484889d3cb291c
Egor <egorzvuzdetskyi@gmail.com>
2018-11-13 15:33:26 +0200

    added Experience component

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import {PropTypes} from 'prop-types';
+import {connect} from "react-redux";
+import {Link, withRouter} from "react-router-dom";
+import Moment from 'react-moment'
+import {
+    deleteExperience
+} from '../../actions/profileActions';
+
+class Experience extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    onDeleteClick = (id) => {
+        this.props.deleteExperience(id, this.props.history);
+    };
+
+    render() {
+
+        const experience = this.props.experience.map(exp => (
+           <tr key={exp._id}>
+               <td>{exp.company}</td>
+               <td>{exp.title}</td>
+               <td>
+                   <Moment format="YYYY/MM/DD">{exp.from}</Moment> -
+                   {exp.to === null ? (' Now') : <Moment fomat="YYYY/MM/DD"> {exp.to}</Moment>}
+               </td>
+               <td><button onClick={this.onDeleteClick.bind(null, exp._id)} className="btn btn-danger">Delete</button></td>
+           </tr>
+        ));
+
+        return(
+            <div>
+                <h4 className="mb-4">Experience Credentials</h4>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Company</th>
+                            <th>Title</th>
+                            <th>Years</th>
+                            <th></th>
+                        </tr>
+                    {experience}
+                    </thead>
+                </table>
+            </div>
+        )
+    }
+}
+
+Experience.propTypes = {
+    deleteExperience: PropTypes.func.isRequired
+};
+
+export default connect(null, {
+    deleteExperience
+})(withRouter(Experience));

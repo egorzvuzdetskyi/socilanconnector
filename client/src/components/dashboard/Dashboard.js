931f009b160d0a77b897821c1aece13436f496df
Egor <egorzvuzdetskyi@gmail.com>
2018-11-13 07:11:32 +0200

    added some new buttons to Dashboard component. added button for delete your profile, imported action from profileActions.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 import {connect} from "react-redux";
-import {getCurrentProfile} from "../../actions/profileActions";
+import { getCurrentProfile, deleteAccount } from "../../actions/profileActions";
 import Spinner from '../common/Spinner'
 import {Link} from "react-router-dom";
+import ProfileActions from "./ProfileActions";
 
 class Dashboard extends Component {
 
     componentDidMount() {
         this.props.getCurrentProfile();
     }
+
+    onDeleteClick = (e) => {
+        e.preventDefault();
+        this.props.deleteAccount();
+    };
 
     render() {
 
@@ -27,12 +33,29 @@ class Dashboard extends Component {
             dashboardContent = <Spinner/>;
         } else {
             if (Object.keys(profile).length > 0) {
-                dashboardContent = <h4>TODO: DISPLAY PROFILE</h4>
+                dashboardContent = (
+                    <div>
+                        <p className="lead text-muted">
+                            Welcome <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
+                        </p>
+                        <ProfileActions/>
+                        {
+                            /*
+                            * TODO: add experience and education*/
+                        }
+                        <div style={{
+                            marginBottom: '60px'
+                        }}/>
+                        <button onClick={this.onDeleteClick} className="btn btn-danger">
+                            Delete my account
+                        </button>
+                    </div>
+                )
             } else {
                 dashboardContent = (
                     <div>
                         <p className="lead text-muted">
-                            Welcome { user.name }
+                            Welcome {user.name}
                         </p>
                         <p>You have not yet set up a profile, please add some info</p>
                         <Link to='create-profile' className="btn btn-lg btn-info">
@@ -62,6 +85,7 @@ class Dashboard extends Component {
 
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
+    deleteAccount: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired
 };
@@ -72,5 +96,6 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, {
-    getCurrentProfile
+    getCurrentProfile,
+    deleteAccount
 })(Dashboard);

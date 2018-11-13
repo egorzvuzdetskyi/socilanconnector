eb755c8edd7906e27c5f2a92086f50357ad45035
Egor <egorzvuzdetskyi@gmail.com>
2018-11-13 07:12:02 +0200

    added delete action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,7 +3,7 @@ import {
     GET_PROFILE,
     PROFILE_LOADING,
     GET_ERRORS,
-    CLEAR_CURRENT_PROFILE
+    CLEAR_CURRENT_PROFILE, SET_CURRENT_USER
 } from "./types";
 
 export const getCurrentProfile = () => dispatch => {
@@ -33,6 +33,27 @@ export const clearProfile = () => {
     return {
         type: CLEAR_CURRENT_PROFILE
     };
+};
+
+export const deleteAccount = () => dispatch => {
+    if(!window.confirm('Are you sure? This can NOT be undone!')) return;
+
+    axios
+        .delete('/api/profile')
+        .then(res => {
+            dispatch({
+                type: SET_CURRENT_USER,
+                payload: {
+
+                }
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        })
 };
 
 export const createProfile = (profileData, history) => dispatch => {

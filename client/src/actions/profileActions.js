47bf6f9e44f0c1ff57c671e5b3ae4f70434ac2eb
Egor <egorzvuzdetskyi@gmail.com>
2018-11-14 06:43:19 +0200

    added method for getting all profiles.

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,7 +3,7 @@ import {
     GET_PROFILE,
     PROFILE_LOADING,
     GET_ERRORS,
-    CLEAR_CURRENT_PROFILE, SET_CURRENT_USER
+    CLEAR_CURRENT_PROFILE, SET_CURRENT_USER, GET_PROFILES
 } from "./types";
 
 export const getCurrentProfile = () => dispatch => {
@@ -121,6 +121,23 @@ export const deleteEducation = (id) => dispatch => {
             dispatch({
                 type: GET_ERRORS,
                 payload: err.response.data
+            })
+        })
+};
+
+export const getProfiles = () => dispatch => {
+    dispatch(setProfileLoading());
+    axios.get('/api/profile/all')
+        .then(res => {
+            dispatch({
+                type: GET_PROFILES,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_PROFILES,
+                payload: null
             })
         })
 };

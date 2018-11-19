62cd99851667d03b2eab8e1e62b9c2872bf21036
Egor <egorzvuzdetskyi@gmail.com>
2018-11-19 10:37:47 +0200

    added logic for getting posts and sending them to PostFeed component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -9,6 +9,8 @@ import PostForm from "./PostForm";
 import {
     Spinner
 } from "../common/index";
+import {getPosts} from "../../actions/postActions";
+import PostFeed from "./PostFeed";
 
 
 class Posts extends Component {
@@ -17,13 +19,28 @@ class Posts extends Component {
         super(props);
     }
 
+    componentDidMount() {
+        this.props.getPosts();
+    }
+
     render() {
+        const {
+            posts,
+            loading
+        } = this.props.post;
+        let postContent;
+        if(posts === null || loading) {
+            postContent = <Spinner/>
+        } {
+            postContent = <PostFeed posts={posts} />
+        }
 
         return (
             <div className="feed">
                 <div className="container col-md-12">
                     <div className="row">
                         <PostForm/>
+                        {postContent}
                     </div>
                 </div>
             </div>
@@ -32,9 +49,14 @@ class Posts extends Component {
 }
 
 Posts.propTypes = {
+    post: PropTypes.object.isRequired,
+    getPosts: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
+    post: state.post
 });
 
-export default connect(mapStateToProps)(Posts);
+export default connect(mapStateToProps, {
+    getPosts
+})(Posts);
